396c4728010c3375f2744b5c68612d465f868d56
superkookai <weerawut@live.com>
2025-03-23 14:29:34 +0700

    Initial Commit

---
+++ ObjectDetection/ObjectDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectDetectionApp.swift
+//  ObjectDetection
+//
+//  Created by Weerawut Chaiyasomboon on 23/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct ObjectDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
